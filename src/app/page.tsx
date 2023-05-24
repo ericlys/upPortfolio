663185ea967234bcb4a3d791cbacff4645a19b98
Ericlys Moreira <ericlysm@gmail.com>
2023-05-24 15:11:18 -0300

    chore: add routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,33 @@
 import Balancer from 'react-wrap-balancer'
 import Particles from '@/components/Particle'
 import { Underline } from '@/components/Underline'
+import Link from 'next/link'
+
+const navigation = [
+  { name: 'Projetos', href: '/projects' },
+  { name: 'Contato', href: '/contact' },
+]
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden">
-      <Particles className="absolute inset-0" quantity={100} />
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <Particles className="absolute inset-0 z-0" quantity={100} />
 
-      <main>
+      <main className="relative z-10">
+        <nav className="z-10 my-16">
+          <ul className="flex items-center justify-center gap-8">
+            {navigation.map((item) => (
+              <li key={item.href}>
+                <Link href={item.href} legacyBehavior>
+                  <a className="text-base text-gray-600 hover:text-purple-700">
+                    {item.name}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
         <h1 className="max-w-4xl text-center text-4xl font-bold leading-[1.1]">
           <Balancer>
             Programador &nbsp;
@@ -21,7 +41,7 @@ export default function Home() {
           </Balancer>
         </h1>
 
-        <div className="mt-4">
+        <div className="mt-16">
           <h2 className="text-center text-gray-500">
             <Balancer>
               Olá, sou Ericlys, um desenvolvedor apaixonado por criar
